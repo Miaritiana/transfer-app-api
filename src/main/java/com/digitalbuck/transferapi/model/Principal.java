a95d2cf63a5d6ff70f17606e7d3be739d78b414c
Miaritiana <hei.miaritiana@gmail.com>
2024-07-15 16:28:05 +0300

    feat: update security

--- src/main/java/com/digitalbuck/transferapi/model/Principal.java
+++ src/main/java/com/digitalbuck/transferapi/model/Principal.java
@@ -1,2 +1,48 @@
-package com.digitalbuck.transferapi.model;public class Principal {
+package com.digitalbuck.transferapi.model;
+
+import com.digitalbuck.transferapi.repository.entity.Account;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Builder
+@AllArgsConstructor
+public record Principal(Account account) implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getTelephone();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
